52a48097c08cd8cacd899513053c0cdc9f40dd14
1001pepi <tanankemr@gmail.com>
2022-09-09 08:22:08 +0100

    feed step 1 completed, sign up, sign in, reset password logic completed 6

--- config/security.js
+++ config/security.js
@@ -30,7 +30,7 @@ module.exports.security = {
 
   cors: {
     allRoutes: true,
-    allowOrigins: '*',
+    allowOrigins: ["https://enspyalumniabroad.herokuapp.com"],
     allowCredentials: false,
   },
 
